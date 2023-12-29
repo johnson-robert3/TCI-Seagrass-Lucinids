d977bc2737f337a40895eb1c193cdc0f6e0f536b
Robert Johnson <johnson.robert.333@gmail.com>
2023-12-29 17:58:52 -0600

    initial commit; read in data and change variable names

---
+++ data_import-and-process.R
@@ -0,0 +1,59 @@
+#--
+# Script to import and process TCI data
+#
+#--
+
+
+library(tidyverse)
+library(readxl)
+
+
+# data
+site_data = read_xlsx(path = "C:/Users/rajohnson6/Box/Turks & Caicos/Seagrass-Lucinid Student DR - F23/Seagrass_Lucinid_DataSheet.xlsx",
+                      sheet = "Metadata")
+
+sg_morphometry = read_xlsx(path = "C:/Users/rajohnson6/Box/Turks & Caicos/Seagrass-Lucinid Student DR - F23/Seagrass_Lucinid_DataSheet.xlsx",
+                           sheet = "Seagrass Morphology")
+
+sg_density = read_xlsx(path = "C:/Users/rajohnson6/Box/Turks & Caicos/Seagrass-Lucinid Student DR - F23/Seagrass_Lucinid_DataSheet.xlsx",
+                       sheet = "Seagrass Data")
+
+core_data = read_xlsx(path = "C:/Users/rajohnson6/Box/Turks & Caicos/Seagrass-Lucinid Student DR - F23/Seagrass_Lucinid_DataSheet.xlsx",
+                      sheet = "Core Data")
+
+lucinid_data = read_xlsx(path = "C:/Users/rajohnson6/Box/Turks & Caicos/Seagrass-Lucinid Student DR - F23/Seagrass_Lucinid_DataSheet.xlsx",
+                         sheet = "Lucinid Measurements")
+
+
+# clean up column names
+site_data = site_data %>% janitor::clean_names() %>%
+  rename(erosion_edge_height_m = vertical_height_of_erosion_edge_m) %>%
+  # add transect position column
+  mutate(transect_position = str_sub(transect_id, start=-5))
+
+sg_morphometry = sg_morphometry %>% janitor::clean_names() %>%
+  rename(transect_distance_m = blade_distance,
+         blade_length_mm = length_mm,
+         blade_width_mm = width_mm)
+
+sg_density = sg_density %>% janitor::clean_names() %>%
+  rename(transect_distance_m = quadrat_distance_from_edge_m,
+         cover_Tt = percent_cover_t_test,
+         cover_Sf = percent_cover_s_fili,
+         cover_Hw = percent_cover_h_wright,
+         cover_sg_tot = total_percent_coverage,
+         cover_algal_tot = total_percent_algal_coverage)
+
+core_data = core_data %>% janitor::clean_names() %>% 
+  rename(transect_distance_m = core_distance,
+         transect_position = above_below_seagrass_edge,
+         live_lucinid_num = number_of_live_lucinids,
+         dead_lucinid_num = number_of_dead_lucinids)
+
+lucinid_data = lucinid_data %>% janitor::clean_names() %>%
+  select(-date, -location_site) %>%  # these variables are already in the site_data df
+  rename(transect_distance_m = core_distance,
+         transect_position = above_below_seagrass_edge,
+         lucinid_length_mm = length_mm)
+
+
