880d197583b15f847a8a1329e2e55dbe4417c263
Robert Johnson <johnson.robert.333@gmail.com>
2024-01-26 16:43:56 -0600

    add fig for total seagrass cover along transects

--- figs_exploration.R
+++ figs_exploration.R
@@ -8,14 +8,27 @@ library(patchwork)
 
 
 # Thalassia coverage along transects
-windows(height=3.5, width=5)
+windows(height=4, width=6)
 ggplot(df %>%
          mutate(transect_distance_m = if_else(transect_position=="below", transect_distance_m * -1, transect_distance_m))) +
   # 
   geom_line(aes(x = transect_distance_m, y = cover_Tt, group = transect_id, color = site_location, linetype = transect_position)) +
   geom_point(aes(x = transect_distance_m, y = cover_Tt, color = site_location, shape = transect_position)) +
   #
-  # scale_x_continuous(name = "Distance (m)", limits = c(0, 10), breaks = seq(1, 9, 2)) +
+  scale_x_continuous(name = "Distance (m)", limits = c(-10, 10), breaks = seq(-10, 10, 2)) +
+  scale_y_continuous(name = "Thalassa % cover") +
+  theme_classic()
+
+
+# Total seagrass coverage along transects
+windows(height=4, width=6)
+ggplot(df %>%
+         mutate(transect_distance_m = if_else(transect_position=="below", transect_distance_m * -1, transect_distance_m))) +
+  # 
+  geom_line(aes(x = transect_distance_m, y = cover_sg_tot, group = transect_id, color = site_location, linetype = transect_position)) +
+  geom_point(aes(x = transect_distance_m, y = cover_sg_tot, color = site_location, shape = transect_position)) +
+  #
+  scale_x_continuous(name = "Distance (m)", limits = c(-10, 10), breaks = seq(-10, 10, 2)) +
   scale_y_continuous(name = "Thalassa % cover") +
   theme_classic()
 
@@ -23,37 +36,33 @@ ggplot(df %>%
 # Live lucinids above vs below
 windows(height=4, width=6)
 ggplot(df %>%
-         summarize(live_lucinid_num = sum(live_lucinid_num), .by = 'transect_position')) +
+         summarize(live_lucinids = sum(live_lucinids, na.rm=TRUE), .by = 'transect_position')) +
   #
-  geom_col(aes(x = transect_position, y = live_lucinid_num)) +
+  geom_col(aes(x = transect_position, y = live_lucinids, fill = transect_position)) +
   #
   theme_classic()
 
 
 # Live lucinids by distance along transect
-windows(height=3.5, width=5)
+windows(height=4, width=6)
 ggplot(df %>%
-         group_by(transect_position, transect_distance_m) %>%
-         summarize(live_lucinid_num = sum(live_lucinid_num)) %>%
-         ungroup() %>%
+         summarize(live_lucinids = sum(live_lucinids), .by = c('transect_position', 'transect_distance_m')) %>%
          mutate(transect_distance_m = if_else(transect_position=="below", transect_distance_m * -1, transect_distance_m))) +
   # 
-  geom_point(aes(x = transect_distance_m, y = live_lucinid_num, color = transect_position), size=2) +
-  geom_line(aes(x = transect_distance_m, y = live_lucinid_num, color = transect_position, group = transect_position)) +
+  geom_point(aes(x = transect_distance_m, y = live_lucinids, color = transect_position), size=2) +
+  geom_line(aes(x = transect_distance_m, y = live_lucinids, color = transect_position, group = transect_position)) +
   #
   theme_classic()
 
 
 # Dead lucinids by distance along transect
-windows(height=3.5, width=5)
+windows(height=4, width=6)
 ggplot(df %>%
-         group_by(transect_position, transect_distance_m) %>%
-         summarize(dead_lucinid_num = sum(dead_lucinid_num)) %>%
-         ungroup() %>%
+         summarize(dead_lucinids = sum(dead_lucinids), .by=c('transect_position', 'transect_distance_m')) %>%
          mutate(transect_distance_m = if_else(transect_position=="below", transect_distance_m * -1, transect_distance_m))) +
   # 
-  geom_point(aes(x = transect_distance_m, y = dead_lucinid_num, color = transect_position), size=2) +
-  geom_line(aes(x = transect_distance_m, y = dead_lucinid_num, color = transect_position, group = transect_position)) +
+  geom_point(aes(x = transect_distance_m, y = dead_lucinids, color = transect_position), size=2) +
+  geom_line(aes(x = transect_distance_m, y = dead_lucinids, color = transect_position, group = transect_position)) +
   #
   theme_classic()
  
@@ -62,7 +71,7 @@ ggplot(df %>%
 windows(height=4, width=6)
 ggplot(df) +
   #
-  geom_point(aes(x = cover_Tt, y = live_lucinid_num, color=transect_position), size=2) +
+  geom_point(aes(x = cover_Tt, y = live_lucinids, color=transect_position), size=2) +
   #
   theme_classic()
 
@@ -71,10 +80,9 @@ ggplot(df) +
 windows(height=4, width=6)
 ggplot(df) +
   #
-  geom_point(aes(x = cover_sg_tot, y = live_lucinid_num, color=transect_position), size=2) +
+  geom_point(aes(x = cover_sg_tot, y = live_lucinids, color=transect_position), size=2) +
   #
   theme_classic()
-
 
 
 
